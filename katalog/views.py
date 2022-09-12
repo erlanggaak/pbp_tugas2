741b81de1cb8a1cd1e50dd064f50061e0fa8db1e
erlanggak <dreakhadafi@gmail.com>
2022-09-12 12:57:22 +0700

    commit changes assignment 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama' : "Erlangga Ahmad Khadafi",
+        'npm'  : "2106750894"
+    }
+    return render(request, "katalog.html", context)
